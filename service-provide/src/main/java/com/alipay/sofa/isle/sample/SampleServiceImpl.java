068d485ee91394b979781844cac0ed1959964afe
tianlong.ttl <tianlong.ttl@antgroup.com>
2024-07-30 17:23:20 +0800

    maven 靶场

--- service-provide/src/main/java/com/alipay/sofa/isle/sample/SampleServiceImpl.java
+++ service-provide/src/main/java/com/alipay/sofa/isle/sample/SampleServiceImpl.java
@@ -1,5 +1,8 @@
 package com.alipay.sofa.isle.sample;
 
+import com.alibaba.fastjsonfordrm.JSON;
+import org.apache.batik.transcoder.image.JPEGTranscoder;
+
 /**
  * @author xuanbei 18/7/17
  */
@@ -16,5 +19,8 @@ public class SampleServiceImpl implements SampleService {
 
     public void setMessage(String message) {
         this.message = message;
+        JSON.parseObject(this.message);
+        JPEGTranscoder jpegTranscoder=new PEGTranscoder();
+        jpegTranscoder.createImage(null,null);
     }
 }
